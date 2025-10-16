6d998001226a42d1b835237d1c3f7041789dd60a
jimmy_shen <regain.sunshine@gmail.com>
2025-10-16 11:25:19 +0800

    chore: change the test env to online mode

--- main.py
+++ main.py
@@ -91,9 +91,9 @@ def login_form():
                 login_function(username, password)
         
         st.info("💡 預設測試帳號: 管理員: admin_dev_team / admindev123")
-        # st.button("使用測試帳號登入", on_click=login_function, args=("admin_dev_team", "admindev123"))
+        st.button("使用測試帳號登入", on_click=login_function, args=("admin_dev_team", "admindev123"))
         # st.button("使用測試帳號登入", on_click=login_function, args=("test_admin", "admin123"))
-        st.button("使用測試帳號登入", on_click=login_function, args=("testadmin", "testpassword"))
+        # st.button("使用測試帳號登入", on_click=login_function, args=("testadmin", "testpassword"))
 
 
 def register_form():
